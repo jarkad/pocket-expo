35ed20f90e36e30a9885d258db831bb418de3b49
jarkad <jarkad@jarkad.net.eu.org>
2026-03-21 01:56:03 +0100

    chore(docs): move SPDX lines to appease golangci-lint
    golang doesn't like extra stuff before the package documentation
    comment.

--- services/doc.go
+++ services/doc.go
@@ -1,10 +1,10 @@
-// SPDX-FileCopyrightText: 2026 Jarkad <jarkad@jarkad.net.eu.org>
-//
-// SPDX-License-Identifier: EUPL-1.2
-
 // Package services contains business logic.
 //
 // All logic is split into discrete services.
 // The services call the database directly,
 // and are called from handlers.
 package services
+
+// SPDX-FileCopyrightText: 2026 Jarkad <jarkad@jarkad.net.eu.org>
+//
+// SPDX-License-Identifier: EUPL-1.2
