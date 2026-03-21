be6482043bbd1abfd200e6ffc20f1eb199e3c13d
jarkad <jarkad@jarkad.net.eu.org>
2026-03-21 01:56:03 +0100

    chore(docs): move SPDX lines to appease golangci-lint
    golang doesn't like extra stuff before the package documentation
    comment.

--- components/doc.go
+++ components/doc.go
@@ -1,10 +1,8 @@
-// SPDX-FileCopyrightText: 2026 Jarkad <jarkad@jarkad.net.eu.org>
-//
-// SPDX-License-Identifier: EUPL-1.2
-
 // Package components contains Templ components.
 //
 // In MVC lingo, it would be views.
-//
-//nolint:godoclint
 package components
+
+// SPDX-FileCopyrightText: 2026 Jarkad <jarkad@jarkad.net.eu.org>
+//
+// SPDX-License-Identifier: EUPL-1.2
